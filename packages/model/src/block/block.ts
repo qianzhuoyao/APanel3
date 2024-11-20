33716e4115de0a6c78e1a7c4636e576a459eda10
1059116864@qq.com <arronqzy@outlook.com>
2024-11-20 17:17:56 +0800

    tips change

--- packages/model/src/block/block.ts
+++ packages/model/src/block/block.ts
@@ -1,10 +1,12 @@
 /**
 The purpose of the block 
-is to map the DOM
-elements on the window into a tree-like structure.
-Any user operations and changes will be synchronized 
-to this structure. It will respond to nodes on the window
-that are bound by a handler, and the node’s redo/undo, 
+is to map the DOM elements
+on the window into a tree-like  
+structure.Any user operations  
+and changes will be synchronized 
+to this structure. It will respond
+to nodes on the window、that are 
+bound by a handler, and the node’s redo/undo, 
 selection state (the state of the handler) will all be mapped 
 to the structure. Changes to this structure will also be synchronized
 back to the DOM, such as modifying relationships, deleting, 
